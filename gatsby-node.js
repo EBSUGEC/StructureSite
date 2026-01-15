dadcfc69bf14a9d76ea40d15b8d617b7bdfabeb6
Félix <someonefefe@gmail.com>
2026-01-15 11:41:55 +0100

    select cover image

--- gatsby-node.js
+++ gatsby-node.js
@@ -39,8 +39,15 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
       value: collection,
     })
 
-    const files = fs.readdirSync(path.dirname(node.fileAbsolutePath)).filter(el => el !== 'index.md')
-    const images = files.filter(el => el.endsWith('.png') || el.endsWith('.jpeg') || el.endsWith('.jpg') || el.endsWith('.webp'))
+    let images = []
+    if(node.frontmatter.image){
+      images = [path.join(path.dirname(node.fileAbsolutePath), node.frontmatter.image)]
+      console.log(images)
+    }
+    else{
+      const files = fs.readdirSync(path.dirname(node.fileAbsolutePath)).filter(el => el !== 'index.md')
+      images = files.filter(el => el.endsWith('.png') || el.endsWith('.jpeg') || el.endsWith('.jpg') || el.endsWith('.webp'))
+    }
     // const sounds = files.filter(el => el.endsWith('.mp3') || el.endsWith('.wav') || el.endsWith('.ogg'))
     
     createNodeField({
